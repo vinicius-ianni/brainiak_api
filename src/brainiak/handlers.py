a30862579e3d67fb3ba7167b098e33b07dd4d8d3
Renan Oliveira <renan.gpa@gmail.com>
2014-08-18 15:06:32 -0300

    Removed cache-control

--- src/brainiak/handlers.py
+++ src/brainiak/handlers.py
@@ -214,7 +214,6 @@ class BrainiakRequestHandler(CorsMixin, RequestHandler):
         # self.finish() -- this is automagically called by greenlet_asynchronous
 
     def finalize_with_cache(self, response, max_age):
-        self.set_header("Cache-control", "private")
         self.set_header("max-age", str(max_age))
 
         self.finalize(response)
